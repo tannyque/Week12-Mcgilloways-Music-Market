b773d2aebd865f43804153758fdd916096ba8f70
tannyque <tannyque1992@gmail.com>
2018-10-05 21:50:31 +0100

    Adds Instrument Type Enumerable

---
+++ src/main/java/instruments/InstrumentType.java
@@ -0,0 +1,12 @@
+package instruments;
+
+public enum InstrumentType {
+
+    WOODWIND("Woodwind"),
+    BRASS("Brass"),
+    STRING("String"),
+    PERCUSSION("Percussion"),
+    KEYBOARD("Keyboard"),
+    GUITAR("Guitar");
+
+}
