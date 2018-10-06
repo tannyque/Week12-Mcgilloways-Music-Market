b31990fc66e6f589873bebd3a1ec11360ff43327
tannyque <tannyque1992@gmail.com>
2018-10-06 12:17:36 +0100

    Adds type

--- src/main/java/instruments/InstrumentType.java
+++ src/main/java/instruments/InstrumentType.java
@@ -9,4 +9,13 @@ public enum InstrumentType {
     KEYBOARD("Keyboard"),
     GUITAR("Guitar");
 
+    private final String type;
+
+    InstrumentType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
