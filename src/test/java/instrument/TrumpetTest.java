01d2076b85b12815ba60e41d02757c4ba745be69
tannyque <tannyque1992@gmail.com>
2018-10-06 14:54:57 +0100

    Adds Trumpet Test

---
+++ src/test/java/instrument/TrumpetTest.java
@@ -0,0 +1,43 @@
+package instrument;
+
+import instruments.InstrumentType;
+import instruments.Trumpet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+    public void setUp() throws Exception {
+        trumpet = new Trumpet("Bach", "Brass", InstrumentType.BRASS, "Gold", 3);
+    }
+    
+    @Test
+    public void hasBrand() {
+        assertEquals("Bach", trumpet.getBrand());
+    }
+
+    @Test
+    public void hasMaterial() {
+        assertEquals("Brass", trumpet.getMaterial());
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("Brass", trumpet.getType());
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("Gold", trumpet.getColour());
+    }
+
+    @Test
+    public void hasNumberOfValves() {
+        assertEquals(3, trumpet.getNumberOfValves());
+    }
+}
