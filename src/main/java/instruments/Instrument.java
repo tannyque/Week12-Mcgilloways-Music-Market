58359344f5458e1da32a4ce06cb132f50888f083
tannyque <tannyque1992@gmail.com>
2018-10-07 18:09:14 +0100

    Refactors abstract classes

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -5,27 +5,16 @@ import shop.Stock;
 
 public abstract class Instrument extends Stock implements IPlay {
 
-    private String brand;
-    private String model;
+
     private String material;
     private InstrumentType type;
     private String colour;
 
     public Instrument(String brand, String model, String material, InstrumentType type, String colour, Double purchasePrice, Double sellingPrice) {
-        super(purchasePrice, sellingPrice);
-        this.brand = brand;
-        this.model = model;
+        super(brand, model, purchasePrice, sellingPrice);
         this.material = material;
         this.type = type;
         this.colour = colour;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getModel() {
-        return model;
     }
 
     public String getMaterial() {
--- src/main/java/shop/Stock.java
+++ src/main/java/shop/Stock.java
@@ -4,12 +4,24 @@ import behaviours.ISell;
 
 public abstract class Stock implements ISell {
 
+    private String brand;
+    private String model;
     private Double purchasePrice;
     private Double sellingPrice;
 
-    public Stock(Double purchasePrice, Double sellingPrice) {
+    public Stock(String brand, String model, Double purchasePrice, Double sellingPrice) {
+        this.brand = brand;
+        this.model = model;
         this.purchasePrice = purchasePrice;
         this.sellingPrice = sellingPrice;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
     }
 
     public Double getPurchasePrice() {
