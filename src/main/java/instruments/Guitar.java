af5e68cd7d076f30cddcf1786bd40abb890f66d6
tannyque <tannyque1992@gmail.com>
2018-10-07 17:54:22 +0100

    Adds guitar

--- src/main/java/instruments/Guitar.java
+++ src/main/java/instruments/Guitar.java
@@ -4,8 +4,8 @@ public class Guitar extends Instrument {
 
     private int numberOfStrings;
 
-    public Guitar(String brand, String model, String material, InstrumentType type, String colour, int numberOfStrings) {
-        super(brand, model, material, type, colour);
+    public Guitar(String brand, String model, String material, InstrumentType type, String colour, Double purchasePrice, Double sellingPrice, int numberOfStrings) {
+        super(brand, model, material, type, colour, purchasePrice, sellingPrice);
         this.numberOfStrings = numberOfStrings;
     }
 
