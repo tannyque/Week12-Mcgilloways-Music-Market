3a33c09fd73cf890625a653d2934d419b74f9ed7
tannyque <tannyque1992@gmail.com>
2018-10-07 17:55:22 +0100

    Adds trumpet :trumpet:

--- src/main/java/instruments/Trumpet.java
+++ src/main/java/instruments/Trumpet.java
@@ -1,20 +1,20 @@
-package instruments;
-
-public class Trumpet extends Instrument {
-
-    private int numberOfValves;
-
-    public Trumpet(String brand, String model, String material, InstrumentType type, String colour, int numberOfValves) {
-        super(brand, model, material, type, colour);
-        this.numberOfValves = numberOfValves;
-    }
-
-    public int getNumberOfValves() {
-        return numberOfValves;
-    }
-
-    @Override
-    public String play() {
-        return "Pah-pa-rah";
-    }
-}
+//package instruments;
+//
+//public class Trumpet extends Instrument {
+//
+//    private int numberOfValves;
+//
+//    public Trumpet(String brand, String model, String material, InstrumentType type, String colour, int numberOfValves) {
+//        super(brand, model, material, type, colour);
+//        this.numberOfValves = numberOfValves;
+//    }
+//
+//    public int getNumberOfValves() {
+//        return numberOfValves;
+//    }
+//
+//    @Override
+//    public String play() {
+//        return "Pah-pa-rah";
+//    }
+//}
--- src/test/java/instrument/TrumpetTest.java
+++ src/test/java/instrument/TrumpetTest.java
@@ -1,53 +1,53 @@
-package instrument;
-
-import instruments.InstrumentType;
-import instruments.Trumpet;
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-public class TrumpetTest {
-
-    Trumpet trumpet;
-
-    @Before
-    public void setUp() throws Exception {
-        trumpet = new Trumpet("Bach", "Mariachi", "Brass", InstrumentType.BRASS, "Gold", 3);
-    }
-    
-    @Test
-    public void hasBrand() {
-        assertEquals("Bach", trumpet.getBrand());
-    }
-
-    @Test
-    public void hasModel() {
-        assertEquals("Mariachi", trumpet.getModel());
-    }
-
-    @Test
-    public void hasMaterial() {
-        assertEquals("Brass", trumpet.getMaterial());
-    }
-
-    @Test
-    public void hasType(){
-        assertEquals("Brass", trumpet.getType());
-    }
-
-    @Test
-    public void hasColour() {
-        assertEquals("Gold", trumpet.getColour());
-    }
-
-    @Test
-    public void hasNumberOfValves() {
-        assertEquals(3, trumpet.getNumberOfValves());
-    }
-
-    @Test
-    public void canPlay() {
-        assertEquals("Pah-pa-rah", trumpet.play());
-    }
-}
+//package instrument;
+//
+//import instruments.InstrumentType;
+//import instruments.Trumpet;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//public class TrumpetTest {
+//
+//    Trumpet trumpet;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        trumpet = new Trumpet("Bach", "Mariachi", "Brass", InstrumentType.BRASS, "Gold", 3);
+//    }
+//
+//    @Test
+//    public void hasBrand() {
+//        assertEquals("Bach", trumpet.getBrand());
+//    }
+//
+//    @Test
+//    public void hasModel() {
+//        assertEquals("Mariachi", trumpet.getModel());
+//    }
+//
+//    @Test
+//    public void hasMaterial() {
+//        assertEquals("Brass", trumpet.getMaterial());
+//    }
+//
+//    @Test
+//    public void hasType(){
+//        assertEquals("Brass", trumpet.getType());
+//    }
+//
+//    @Test
+//    public void hasColour() {
+//        assertEquals("Gold", trumpet.getColour());
+//    }
+//
+//    @Test
+//    public void hasNumberOfValves() {
+//        assertEquals(3, trumpet.getNumberOfValves());
+//    }
+//
+//    @Test
+//    public void canPlay() {
+//        assertEquals("Pah-pa-rah", trumpet.play());
+//    }
+//}
